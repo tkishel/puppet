1f4ae7504df3d6c9f4378f56a44634c1b7e5dad9
tkishel <tkishel@gmail.com>
2017-10-16 12:31:39 -0700

    (PUP-6134) Explicitly locate gemcmd windows
    On Windows ...
    
    Puppet is unable to install/inspect a ruby gem other than in Puppet's ruby.
    Caused by Puppet prepending its paths (including our RUBY_DIR) to PATH.
    This fix removes Puppet's RUBY_DIR from PATH when setting :gemcmd.

--- lib/puppet/provider/package/gem.rb
+++ lib/puppet/provider/package/gem.rb
@@ -17,7 +17,21 @@ Puppet::Type.type(:package).provide :gem, :parent => Puppet::Provider::Package d
 
   has_feature :versionable, :install_options, :uninstall_options
 
-  commands :gemcmd => "gem"
+  if Puppet.features.microsoft_windows?
+    # Puppet on Windows prepends its paths to PATH.
+    # One of those paths includes Puppet's RUBY_DIR.
+    # That results in the which() method in self.command(name) in
+    # lib/puppet/provider.rb always returning Puppet's ruby 'gem' command.
+    # To prevent this, call which('gem') with a PATH without Puppet's RUBY_DIR,
+    # and set :gemcmd to the full-path result.
+    ruby_bin_dir = Puppet::Util.get_env('RUBY_DIR') + '\bin'
+    path_array = Puppet::Util.get_env('PATH').split(';')
+    path_array.reject! {|p| p == ruby_bin_dir}
+    path_without_puppet_ruby_bin_dir = path_array.join(';')
+    commands :gemcmd => Puppet::Util.withenv({:PATH => path_without_puppet_ruby_bin_dir}, :windows) { which('gem') }
+  else
+    commands :gemcmd => "gem"
+  end
 
   def self.gemlist(options)
     gem_list_command = [command(:gemcmd), "list"]
